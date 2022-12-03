b5af2fc8786cdbf538c0302495ade76c3da941b3
Shawn O'Hare <shawn@shawnohare.com>
2022-12-02 16:56:49 -0800

    Increase dark color lightness to 65.
    Bump the brightness of the dark variant colors to 65.
    
    Define additional colors for lightness values 50 and 70. These are
    presently unused.

--- lua/lush_theme/hadalized.lua
+++ lua/lush_theme/hadalized.lua
@@ -47,109 +47,133 @@ local hsluv = lush.hsluv
 -- Colors are drawn from the okhsl(h, 100, l) color space
 -- ---------------------------------------------------------------------------
 --
+-- okhsl hue values for color.
+-- local hues = {
+--     red     = 025,
+--     orange  = 055,
+--     yellow  = 110,
+--     green   = 130,
+--     spring  = 155,
+--     cyan    = 185,
+--     blue    = 245,
+--     violet  = 290,
+--     magenta = 330,
+--     cerise  = 345,
+--     rose    = 360,
+-- },
 -- okhsl(h, 100, l).
 -- Static colors used in both light and dark modes.
 local colors = {
-    -- okhsl hue values for color.
-    -- hues = {
-    --     red     = 025,
-    --     orange  = 055,
-    --     yellow  = 110,
-    --     green   = 130,
-    --     spring  = 155,
-    --     cyan    = 185,
-    --     blue    = 245,
-    --     violet  = 290,
-    --     magenta = 330,
-    --     cerise  = 345,
-    --     rose    = 360,
-    -- },
-    -- okhsl(h, 100, 55)
-    l55 = {
-        red     = hsluv('#f60029'),
-        orange  = hsluv('#c76600'),
-        yellow  = hsluv('#898a00'),
-        green   = hsluv('#629600'), -- h=130
-        spring  = hsluv('#009d5a'), -- h=155
-        cyan    = hsluv('#00988c'),
-        blue    = hsluv('#008ad9'),
-        violet  = hsluv('#885eff'),
-        magenta = hsluv('#d700d0'), -- h=330
-        cerise  = hsluv('#e400a6'),
-        rose    = hsluv('#ed007e'), -- h=360
-        -- green   = hsluv('#4f9a00'),
-        -- green   = hsluv('#009e48'), -- h=150
-        -- violet  = hsluv('#716aff'),
-    },
-    -- okhsl(h, 100, 60)
-    l60 = {
-        red     = hsluv('#ff3b41'),
-        orange  = hsluv('#da7000'),
-        yellow  = hsluv('#979700'),
-        green   = hsluv('#6ca500'), -- h=130,
-        spring  = hsluv('#00ad63'), -- h=155,
-        cyan    = hsluv('#00a79a'),  -- h=185
-        blue    = hsluv('#0098ee'),
-        violet  = hsluv('#9374ff'),
-        magenta = hsluv('#eb00e4'),
-        cerise  = hsluv('#f900b7'), -- h=345
-        rose    = hsluv('#ff1f8b'),
-        -- green   = hsluv('#57a900'), -- h=135
-        -- green   = hsluv('#00af2f'), -- h=145,
-        -- green   = hsluv('#00ae50'), -- h=150
-    },
-    -- okhsl(h, 100, 65)
-    l65 = {
-        red = hsluv('#ff625e'),
-        orange = hsluv('#ed7b00'),
-        yellow = hsluv('#a5a500'),
-        green  = hsluv('#76b300'),
-        spring  = hsluv('#00bc6c'), -- h=155
-        cyan   = hsluv('#00b6a8'),
-        blue   = hsluv('#1aa5ff'),
-        violet = hsluv('#9e88ff'),
-        magenta = hsluv('#ff0bf7'),
-        rose    = hsluv('#ff5699'),
-    },
-    -- okhsl(h, 100, 75) for backgrounds
-    l75 = {
-        red     = hsluv('#ff9890'),
-        orange  = hsluv('#ff9e57'),
-        yellow  = hsluv('#c1c100'),
-        green   = hsluv('#8ad200'), -- h=130
-        spring  = hsluv('#00dc7f'), -- h=155
-        cyan    = hsluv('#00d5c5'),
-        blue    = hsluv('#75c1ff'),
-        violet  = hsluv('#b8acff'),
-        magenta = hsluv('#ff82f6'),
-        cerise  = hsluv('#ff8cd0'),
-        rose    = hsluv('#ff92b5'),
-    },
-    -- okhsl(h, 100, 80) for backgrounds
-    l80 = {
-        red     = hsluv('#ffafa7'),
-        orange  = hsluv('#ffb37f'),
-        yellow  = hsluv('#cfd000'),
-        green   = hsluv('#95e100'),
-        spring  = hsluv('#00ec89'),
-        cyan    = hsluv('#00e5d3'),
-        blue    = hsluv('#94ceff'),
-        violet  = hsluv('#c5bdff'),
-        magenta = hsluv('#ff9ff7'),
-        rose    = hsluv('#ffaac4'),
-    },
-    -- okhsl(h, 100, 85)
-    l85 = {
-        red     = hsluv('#ffc4be'),
-        orange  = hsluv('#ffc7a2'),
-        yellow  = hsluv('#ddde00'),
-        green   = hsluv('#9ff100'),
-        spring  = hsluv('#00fc93'),
-        cyan    = hsluv('#00f3ea'),
-        blue    = hsluv('#b0daff'),
-        violet  = hsluv('#d3ceff'),
-        magenta = hsluv('#ffbaf8'),
-        rose    = hsluv('#ffc1d2'),
+    s100 = {
+        l50 = {
+            red     = hsluv('#df0024'),
+            orange  = hsluv('#b45c00'),
+            yellow  = hsluv('#7c7d00'),
+            green   = hsluv('#588800'),
+            spring  = hsluv('#008e50'),
+            cyan    = hsluv('#008a7f'),
+            blue    = hsluv('#007dc5'),
+            violet  = hsluv('#7e43ff'),
+            magenta = hsluv('#c300bd'),
+            cerise  = hsluv('#cf0097'),
+            rose    = hsluv('#d70072'),
+
+        },
+        l55 = {
+            red     = hsluv('#f60029'),
+            orange  = hsluv('#c76600'),
+            yellow  = hsluv('#898a00'),
+            green   = hsluv('#629600'),
+            spring  = hsluv('#009d5a'),
+            cyan    = hsluv('#00988c'),
+            blue    = hsluv('#008ad9'),
+            violet  = hsluv('#885eff'),
+            magenta = hsluv('#d700d0'),
+            cerise  = hsluv('#e400a6'),
+            rose    = hsluv('#ed007e'),
+        },
+        -- okhsl(h, 100, 60)
+        l60 = {
+            red     = hsluv('#ff3b41'),
+            orange  = hsluv('#da7000'),
+            yellow  = hsluv('#979700'),
+            green   = hsluv('#6ca500'),
+            spring  = hsluv('#00ad63'),
+            cyan    = hsluv('#00a79a'),
+            blue    = hsluv('#0098ee'),
+            violet  = hsluv('#9374ff'),
+            magenta = hsluv('#eb00e4'),
+            cerise  = hsluv('#f900b7'),
+            rose    = hsluv('#ff1f8b'),
+            -- green   = hsluv('#57a900'), -- h=135
+            -- green   = hsluv('#00af2f'), -- h=145,
+            -- green   = hsluv('#00ae50'), -- h=150
+        },
+        -- okhsl(h, 100, 65)
+        l65 = {
+            red     = hsluv('#ff625e'),
+            orange  = hsluv('#ed7b00'),
+            yellow  = hsluv('#a5a500'),
+            green   = hsluv('#76b300'),
+            spring  = hsluv('#00bc6c'),
+            cyan    = hsluv('#00b6a8'),
+            blue    = hsluv('#1aa5ff'),
+            violet  = hsluv('#9e88ff'),
+            magenta = hsluv('#ff0bf7'),
+            rose    = hsluv('#ff5699'),
+        },
+        l70 = {
+            red     = hsluv('#ff7f78'),
+            orange  = hsluv('#ff8610'),
+            yellow  = hsluv('#b3b300'),
+            green   = hsluv('#80c200'),
+            spring  = hsluv('#00cc76'),
+            cyan    = hsluv('#00c6b6'),
+            blue    = hsluv('#52b3ff'),
+            violet  = hsluv('#ab9aff'),
+            magenta = hsluv('#ff5cf6'),
+            rose    = hsluv('#ff77a7'),
+        },
+        -- okhsl(h, 100, 75) for backgrounds
+        l75 = {
+            red     = hsluv('#ff9890'),
+            orange  = hsluv('#ff9e57'),
+            yellow  = hsluv('#c1c100'),
+            green   = hsluv('#8ad200'),
+            spring  = hsluv('#00dc7f'),
+            cyan    = hsluv('#00d5c5'),
+            blue    = hsluv('#75c1ff'),
+            violet  = hsluv('#b8acff'),
+            magenta = hsluv('#ff82f6'),
+            cerise  = hsluv('#ff8cd0'),
+            rose    = hsluv('#ff92b5'),
+        },
+        -- okhsl(h, 100, 80) for backgrounds
+        l80 = {
+            red     = hsluv('#ffafa7'),
+            orange  = hsluv('#ffb37f'),
+            yellow  = hsluv('#cfd000'),
+            green   = hsluv('#95e100'),
+            spring  = hsluv('#00ec89'),
+            cyan    = hsluv('#00e5d3'),
+            blue    = hsluv('#94ceff'),
+            violet  = hsluv('#c5bdff'),
+            magenta = hsluv('#ff9ff7'),
+            rose    = hsluv('#ffaac4'),
+        },
+        -- okhsl(h, 100, 85)
+        l85 = {
+            red     = hsluv('#ffc4be'),
+            orange  = hsluv('#ffc7a2'),
+            yellow  = hsluv('#ddde00'),
+            green   = hsluv('#9ff100'),
+            spring  = hsluv('#00fc93'),
+            cyan    = hsluv('#00f3ea'),
+            blue    = hsluv('#b0daff'),
+            violet  = hsluv('#d3ceff'),
+            magenta = hsluv('#ffbaf8'),
+            rose    = hsluv('#ffc1d2'),
+        },
     },
 }
 
@@ -324,10 +348,10 @@ local palettes = {
         fg2 = base.neutral.l35,
         fg1 = base.neutral.l28,
         fg0 = base.neutral.l05,
-        dim = colors.l55,
-        color = colors.l60,
-        br = colors.l65,
-        hl = colors.l85,
+        dim = colors.s100.l50,
+        color = colors.s100.l55,
+        br = colors.s100.l65,
+        hl = colors.s100.l85,
     },
     dark = {
         bg0 = base.cool.l08,
@@ -341,10 +365,10 @@ local palettes = {
         fg2 = base.neutral.l65,
         fg1 = base.neutral.l75,
         fg0 = base.neutral.l90,
-        dim = colors.l55,
-        color = colors.l55,
-        br = colors.l75,
-        hl = colors.l85,
+        dim = colors.s100.l55,
+        color = colors.s100.l65,
+        br = colors.s100.l75,
+        hl = colors.s100.l85,
     }
 }
 
