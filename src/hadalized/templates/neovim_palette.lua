a0d1750a06ecbe7d64048fd0bb369dc03b81c207
Shawn O'Hare <shawn@shawnohare.com>
2026-01-20 18:35:33 -0800

    Move hadalized python app to separate package.
    Place python theme generation code into a separate git repository, since
    that application handles more than just neovim themes.
    
    Update just to assume that the `hadalized` git repo is included.

--- src/hadalized/templates/neovim_palette.lua
+++
@@ -1,7 +0,0 @@
--- Generated file for a neovim hadalized data in lua/hadalized/palettes/
--- Load via require("hadalized.palettes" .. "{{name}}")
--- WARNING: Do not edit by hand.
-
-local M = {{data}}
-
-return M
